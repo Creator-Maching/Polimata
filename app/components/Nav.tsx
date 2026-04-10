163cc9ef22870ba3875997b3cb46d902101c36fe
Pedro Dias Mendes Vianna <ordepmeta@gmail.com>
2026-04-10 16:22:20 -0300

    Update Nav.tsx

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,29 +2,42 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="text-2xl font-bold bg-blue-500 text-white rounded m-3 p-3">
-      <ul className="flex items-center justify-center space-x-4 m-3 p-3">
+    <nav className="bg-blue-500 text-white rounded m-3 p-3">
+      
+      <ul className="
+        flex 
+        flex-col 
+        sm:flex-row 
+        items-center 
+        justify-center 
+        gap-2 
+        sm:gap-4
+        text-lg 
+        sm:text-xl 
+        md:text-2xl 
+        font-bold
+      ">
 
-        <li>
-          <Link href="/" className="hover:bg-black rounded p-3">
+        <li className="w-full sm:w-auto text-center">
+          <Link href="/" className="block hover:bg-black rounded p-2 sm:p-3">
             Home
           </Link>
         </li>
 
-        <li>
-          <Link href="/world" className="hover:bg-black rounded p-3">
+        <li className="w-full sm:w-auto text-center">
+          <Link href="/world" className="block hover:bg-black rounded p-2 sm:p-3">
             Material
           </Link>
         </li>
 
-        <li>
-          <Link href="/mind" className="hover:bg-black rounded p-3">
+        <li className="w-full sm:w-auto text-center">
+          <Link href="/mind" className="block hover:bg-black rounded p-2 sm:p-3">
             Mental
           </Link>
         </li>
 
-        <li>
-          <Link href="/technology" className="hover:bg-black rounded p-3">
+        <li className="w-full sm:w-auto text-center">
+          <Link href="/technology" className="block hover:bg-black rounded p-2 sm:p-3">
             Tecnológico
           </Link>
         </li>
